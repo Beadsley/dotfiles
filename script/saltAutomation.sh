f94b46f9ef57a032e6824886995fe19f686d59fd
beadsley <danbeadleson@gmail.com>
2024-01-15 14:18:38 +0100

    init

---
+++ script/saltAutomation.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+echo ">>>>Updating OSX stuff"
+sudo softwareupdate -i -a
+sudo spctl --master-disable ## Disables security stuff in catalina that will drive the devs crazy
+
+echo ">>>>>Installing homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)" </dev/null ## /dev/null skips pressing enter for the installation
+
+echo ">>>>>Bootstrapping all the applications for the first time, overwriting all"
+git clone https://github.com/appliedtechnology/salt-jsfs-dotfiles.git ~/.dotfiles
+sh ~/.dotfiles/script/bootstrap_overwriteAll
+
+echo ">>>>>Setting correct version of node for the course"
+. ~/.dotfiles/nvm/nvm.zsh
+nvm install v12.13
+nvm alias default v12.13
+nvm use default
