072a6ddba7c7579cbd13b0fdc569ebb8b1bc6c11
beadsley <danbeadleson@gmail.com>
2024-01-16 13:55:01 +0100

    lint and next aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,6 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+alias nev='npm run dev' # run next server
+alias lint='npm run lint' # run lint
+alias lint-t='npm run lint:tsc' # run lint tsc
