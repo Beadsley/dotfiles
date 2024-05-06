20f442b0a0ea7f0578d1a912fd3af9bbabca1f56
Beadsley  <danbeadleson@gmail.com>
2024-05-06 15:27:33 +0200

    uddated aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,9 +7,9 @@ then
 fi
 
 # The rest of my fun git aliases
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 alias gac='git add -A && git commit -m'
+alias gits='git status -s'
 alias gev='git add . && git rev' # get rid of changes
 alias gappend='git add . && git commit -a --amend --no-edit' ## append changes to last commit
 alias gitrid='git branch | grep -v "dev" | grep -v "staging" | grep -v "main" | grep -v "master" | xargs git branch -D' ## delete all branches except dev, staging, main, master
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,16 @@
 alias reload!='. ~/.zshrc'
 
-alias cls='clear' # Good 'ol Clear Screen command
-alias nev='npm run dev' # run next server
-alias lint='npm run lint' # run lint
-alias lint-t='npm run lint:tsc' # run lint tsc
+### ALIAS
+alias nev='npm run dev'
+alias lint='npm run lint'
+alias lint-t='npm run lint:tsc'
+alias aliases="cat ~/.alias"
+alias 'lsa'='ls -a -1'
+
+### cmd
+alias cmd-docker="mdless ~/cmd-docs/docker/docker.md"
+alias cmd-git="mdless ~/cmd-docs/git/git.md"
+alias cmd-cli="mdless ~/cmd-docs/cli/cli.md"
+alias cmd-psql="mdless ~/cmd-docs/psql/psql.md"
+alias cmd-portal="mdless ~/cmd-docs/lp-portal.md"
+alias cmd-shortcuts="mdless ~/cmd-docs/shortcuts/shortcuts.md"
